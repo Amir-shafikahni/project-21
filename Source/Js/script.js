38928fa1ca116337b4cca9f1e9ec4671a2537a43
Amir_sh <mrshfkhn@gmail.com>
2022-07-06 21:47:44 +0430

    bug fixed

--- Source/Js/script.js
+++ Source/Js/script.js
@@ -1,62 +1,48 @@
 let $ = document
 
-////////////////////////////
+/////////////////////////////
 
-// variables //////////////
-const body = $.body
-const imgElem = $.querySelector(".img-fluid")
-const nextBtn = $.querySelector(".fa-angles-right")
-const prevBtn = $.querySelector(".fa-angles-left")
+// variables ///////////////
+const carouselSlide = $.querySelector(".carousel-slide")
+const carouselImages = $.querySelectorAll(".carousel-slide img")
+const nextBtn = $.querySelector(".nextBtn")
+const prevBtn = $.querySelector(".prevBtn")
 
-const allImgSrc = [
-    "./Source/Media/Images/pic-1.jpg",
-    "./Source/Media/Images/pic-2.jpg",
-    "./Source/Media/Images/pic-3.jpg",
-    "./Source/Media/Images/pic-4.jpg"
-]
-let index = 0
+//counter
+let counter = 1    
+const size = carouselImages[0].clientWidth
 
+carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
 
-// fucntions ///////////////
-// to get user screen min height every 0.1s
-function liveUserScreenMinHeight(){
-    let screenMinHeight = visualViewport.height
-    body.style.minHeight = screenMinHeight + "px"
+// function /////////////////
+function nextImage (){
+    if (counter >= carouselImages.length -1) return
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter++
+    carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
 }
 
-// to move to the next img by clicking on next button
-function nextImg(){
-    index++
+function prevImage (){
+    if(counter <= 0) return
+    carouselSlide.style.transition = "transform 0.4s ease-in-out"
+    counter--
+    carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
+}
 
-    if(index > allImgSrc.length - 1){
-        index = 0
+function stayInCarouselLoop (){
+    if(carouselImages[counter].id === "lastClone"){
+        carouselSlide.style.transition = 'none'
+        counter = carouselImages.length - 2
+        carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
+    }else if(carouselImages[counter].id === "firstClone"){
+        carouselSlide.style.transition = 'none'
+        counter = carouselImages.length - counter
+        carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
     }
-
-    changeImg(index)
-} 
-
-// to move to the previous img by clicking on prev button
-function prevImg (){   
-    index--
-
-    if(index < 0){
-       index = allImgSrc.length - 1
-    }
-
-    changeImg(index)
 }
 
-// changing background animation
-function changeImg(srcIndex){
-    imgElem.classList.add("fade-out")
-    setTimeout(function(){
-        imgElem.classList.remove("fade-out")
-    },150)    
-    imgElem.src = allImgSrc[srcIndex]
-}
-
-// event listeners //////////
-setInterval(liveUserScreenMinHeight, 100)
-setInterval(nextImg , 5000)
-nextBtn.addEventListener("click" , nextImg)
-prevBtn.addEventListener("click" , prevImg)
+// event listeners ////////////////
+nextBtn.addEventListener("click" , nextImage)
+prevBtn.addEventListener("click" , prevImage)
+carouselSlide.addEventListener("transitionend" , stayInCarouselLoop)
+setInterval(nextImage , 4000)
