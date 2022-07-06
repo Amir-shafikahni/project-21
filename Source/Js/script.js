84c5719b8e880d753b09e5a590359c45349e995b
Amir_sh <mrshfkhn@gmail.com>
2022-07-06 21:51:26 +0430

    all bugs fixed

--- Source/Js/script.js
+++ Source/Js/script.js
@@ -15,20 +15,28 @@ const size = carouselImages[0].clientWidth
 carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
 
 // function /////////////////
+
+// to go to the next image
 function nextImage (){
+    // to avoid leaving the loop when clicking on next btn very fast
     if (counter >= carouselImages.length -1) return
+
     carouselSlide.style.transition = "transform 0.4s ease-in-out"
     counter++
     carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
 }
 
+// to go to the previous image
 function prevImage (){
+    // to avoid leaving the loop when clicking on previous btn very fast
     if(counter <= 0) return
+
     carouselSlide.style.transition = "transform 0.4s ease-in-out"
     counter--
     carouselSlide.style.transform = "translateX(" + -size * counter  + "px)" 
 }
 
+// to make a loop for the carousel
 function stayInCarouselLoop (){
     if(carouselImages[counter].id === "lastClone"){
         carouselSlide.style.transition = 'none'
